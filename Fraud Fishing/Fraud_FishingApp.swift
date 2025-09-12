ddbaaf59d5c035ea2fd5fa465516b524ea70ceb4
JavCan <javicanella69@gmai.com>
2025-09-11 19:33:44 -0600

    Initial Commit

---
+++ Fraud Fishing/Fraud_FishingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fraud_FishingApp.swift
+//  Fraud Fishing
+//
+//  Created by Javier Canella Ramos on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Fraud_FishingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
