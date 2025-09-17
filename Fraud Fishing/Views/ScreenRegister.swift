f488727d8fae417ed17d3bfca2066d68dd0a9d2f
JavCan <javicanella69@gmai.com>
2025-09-16 23:00:59 -0600

    feat: add initial ScreenRegister view for Fraud Fishing

---
+++ Fraud Fishing/Views/ScreenRegister.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenRegister.swift
+//  Fraud Fishing
+//
+//  Created by Javier Canella Ramos on 16/09/25.
+//
+
+import SwiftUI
+
+struct ScreenRegister: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenRegister()
+}
