7f09c0c2f4718809f8c336b7d88bef3834bbfd1e
JavCan <javicanella69@gmai.com>
2025-10-14 17:34:08 -0600

    feat: add new ScreenEdgar view and remove old onboarding screens
    - Introduce new ScreenEdgar view with report cards and category selector
    - Remove deprecated ScreenOnboarding2 and ScreenOnboarding3 views

---
+++ Fraud Fishing/Controllers/ScreenEdgar.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenEdgar.swift
+//  Fraud Fishing
+//
+//  Created by Javier Canella Ramos on 13/10/25.
+//
+
+import SwiftUI
+
+struct ScreenEdgar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenEdgar()
+}
