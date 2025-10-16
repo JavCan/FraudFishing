d9a7ce1513654fd266c84b743d55fc5b223b5eb1
Ferro <a01659298@tec.mx>
2025-10-15 22:12:56 -0600

    Modifique login y registro para usar tokens y agregue storage de tokens

---
+++ Fraud Fishing/Networking/AuthRequest.swift
@@ -0,0 +1,8 @@
+//
+//  AuthRequest.swift
+//  Fraud Fishing
+//
+//  Created by Usuario on 15/10/2025.
+//
+
+import Foundation
---
+++ Fraud Fishing/Storage/StorageTokens.swift
@@ -0,0 +1,8 @@
+//
+//  StorageTokens.swift
+//  Fraud Fishing
+//
+//  Created by Usuario on 15/10/2025.
+//
+
+import Foundation
