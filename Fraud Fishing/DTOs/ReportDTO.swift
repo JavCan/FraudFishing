57e6187c2fd076c792492d29a84a98013c4b7176
d <usuario@iMac.local>
2025-10-17 22:55:04 -0600

    reportes pendientes y aceptados

--- Fraud Fishing/DTOs/ReportDTO.swift
+++ Fraud Fishing/DTOs/ReportDTO.swift
@@ -50,3 +50,14 @@ struct ServerErrorResponse: Decodable {
     let error: String?
     let statusCode: Int
 }
+
+// MARK: - Tag Response DTO
+struct TagResponse: Codable {
+    let id: Int
+    let name: String
+}
+
+// MARK: - Tags Array Response
+// Si el endpoint devuelve un array directamente
+typealias TagsResponse = [TagResponse]
+
